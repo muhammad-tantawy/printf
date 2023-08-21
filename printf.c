a779c61e47521bbdb0302446bed5b1b74d941600
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-21 11:19:17 -0700

    fixing-comments

--- printf.c
+++ printf.c
@@ -2,12 +2,53 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+/**
+ *print_char - function prints characters
+ *@buffer: variable used to store arg
+ *@index: variable used as index
+ *@args: arguments of the vriadic function
+ *Return: returns 1
+ */
+int print_char(char **buffer, int *index, va_list args)
+{
+(*buffer)[(*index)++] = va_arg(args, int);
+return (1);
+}
+/**
+ *print_string - function prints string
+ *@buffer: variable used to store arg
+ *@index: variable used as index
+ *@args: arguments of the vriadic function
+ *Return: returns counts of int
+ */
+int print_string(char **buffer, int *index, va_list args)
+{
+char *str = va_arg(args, char *);
+int count = 0;
+for (int i = 0; str[i] != '\0'; i++)
+{
+(*buffer)[(*index)++] = str[i];
+count++;
+}
+return (count);
+}
+/**
+ *print_percent - function prints percentage
+ *@buffer: variable used to store arg
+ *@index: variable used as index
+ *Return: returns 1
+ */
+
 int print_percent(char **buffer, int *index)
 {
 (*buffer)[(*index)++] = '%';
 return (1);
 }
-
+/**
+ *_printf - function mimics printf
+ *@format: variable holds the format specifier
+ *Return: returns number of charcaters printed
+ */
 int _printf(const char *format, ...)
 {
 va_list args;
