a750edfdd49613961e0a1c121d4de888c5e23404
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-21 11:46:28 -0700

    fixed-compilation-errors

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 int print_char(char **buffer, int *index, va_list args);
 int _printf(const char *format, ...);
 int print_string(char **buffer, int *index, va_list args);
--- printf.c
+++ printf.c
@@ -11,8 +11,8 @@
  */
 int print_char(char **buffer, int *index, va_list args)
 {
-(*buffer)[(*index)++] = va_arg(args, int);
-return (1);
+	(*buffer)[(*index)++] = va_arg(args, int);
+	return (1);
 }
 /**
  *print_string - function prints string
@@ -23,14 +23,15 @@ return (1);
  */
 int print_string(char **buffer, int *index, va_list args)
 {
-char *str = va_arg(args, char *);
-int count = 0;
-for (int i = 0; str[i] != '\0'; i++)
-{
-(*buffer)[(*index)++] = str[i];
-count++;
-}
-return (count);
+	char *str = va_arg(args, char *);
+	int count = 0;
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		(*buffer)[(*index)++] = str[i];
+		count++;
+	}
+	return (count);
 }
 /**
  *print_percent - function prints percentage
@@ -41,8 +42,8 @@ return (count);
 
 int print_percent(char **buffer, int *index)
 {
-(*buffer)[(*index)++] = '%';
-return (1);
+	(*buffer)[(*index)++] = '%';
+	return (1);
 }
 /**
  *_printf - function mimics printf
@@ -51,53 +52,60 @@ return (1);
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
-int buffer_size = 1024;
-char *buffer = malloc(buffer_size);
-if (!buffer)
-return (-1);
-int index = 0;
-int count = 0;
-for (int i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-i++;
-switch (format[i])
-{
-case 'c':
-count += print_char(&buffer, &index, args);
-break;
-case 's':
-count += print_string(&buffer, &index, args);
-break;
-case '%':
-count += print_percent(&buffer, &index);
-break;
-default:
-break;
-}
-}
-else
-{
-buffer[index++] = format[i];
-count++;
-}
-if (index >= buffer_size - 1)
-{
-buffer_size *= 2;
-char *new_buffer = realloc(buffer, buffer_size);
-if (!new_buffer)
-{
-free(buffer);
-return (-1);
-}
-buffer = new_buffer;
-}
-}
-write(1, buffer, index);
-free(buffer);
-va_end(args);
-return (count);
+	int buffer_size = 1024;
+	int index = 0;
+	char *new_buffer;
+	va_list args;
+	char *buffer;
+	int count = 0;
+	int i;
+	
+	va_start(args, format);
+	va_start(args, format);
+       	buffer = malloc(buffer_size);
+
+       	buffer = malloc(buffer_size);
+	if (!buffer)
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+					{
+						case 'c':
+							count += print_char(&buffer, &index, args);
+							break;
+						case 's':
+							count += print_string(&buffer, &index, args);
+							break;
+						case '%':
+							count += print_percent(&buffer, &index);
+							break;
+						default:
+							break;
+					}
+		}
+		else
+		{
+			buffer[index++] = format[i];
+			count++;
+		}
+		if (index >= buffer_size - 1)
+		{
+			buffer_size *= 2;
+			new_buffer = realloc(buffer, buffer_size);
+			if (!new_buffer)
+			{
+				free(buffer);
+				return (-1);
+			}
+			buffer = new_buffer;
+		}
+	}
+	write(1, buffer, index);
+	free(buffer);
+	va_end(args);
+	return (count);
 }
