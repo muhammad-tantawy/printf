cd19e139082f3283fafa760fca1dc41a66bcbcba
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-21 11:22:39 -0700

    fixing

--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int print_char(char **buffer, int *index, va_list args);
 int _printf(const char *format, ...);
+int print_string(char **buffer, int *index, va_list args);
+int print_percent(char **buffer, int *index);
 #endif
